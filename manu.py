f9493cec5659461414a010dded165de46bfacd02
sohel <sohelahmadkhan000@gmail.com>
2018-01-19 11:17:29 +0530

    Manu file upload

---
+++ manu.py
@@ -0,0 +1,24 @@
+def add(a,b):
+	sum = a + b
+	return sum
+
+def sub(a,b):
+	sub = a - b
+	return sub
+
+while True:
+	print " 1. Addition \n 2. Subtraction \n 3. Exit"
+	n = int(raw_input('Enter your choice : '))
+	
+	if not n == 3: 
+        	a = int(raw_input("enter value of A : "))
+	    	b = int(raw_input("enter value of b : "))
+
+	if n == 1:
+		print  'Addition is :',add(a,b)
+	elif n == 2:
+		print 'Subtraction is : ', sub(a,b)
+	else:
+		break
+
+
